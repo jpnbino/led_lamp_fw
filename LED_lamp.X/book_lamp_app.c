853a877449fe2f74774458c6c20d4c57e602e9a2
jpnbino <jpnogg@gmail.com>
2020-05-09 17:33:21 +0200

    Corrected size of array Added Book_Lamp_Set_Light

--- LED_lamp.X/book_lamp_app.c
+++ LED_lamp.X/book_lamp_app.c
@@ -2,10 +2,11 @@
 #include "book_lamp_app.h"
 #include "driver_button.h"
 #include "soft_pwm.h"
-#include "mcc_generated_files/delay.h"
 #include "systick.h"
 #include "button_events.h"
 #include "light.h"
+
+#define ARRAYSIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 
 static uint8_t brightness;
 uint8_t const set_brightness[5] = {30,80,120,200,255};
@@ -35,10 +36,22 @@ typedef enum {
     EVENT_CHANGE_BRIGHTNESS
 }event_t;
 
+void Book_Lamp_Set_Light( light_t light)
+{
+    static uint8_t white;
+    static uint8_t yellow;
+    
+    light_brightness.white = (brightness * light_ratio.white)/ ( light_ratio.white + light_ratio.yellow);
+            
+    light_brightness.yellow = (brightness * light_ratio.yellow)/ ( light_ratio.white + light_ratio.yellow);
+    
+    Set_Light_Brightness(light_brightness);   
+}
+
 void Event_Turn_On_Handler ( void )
 {
     light_t light = {brightness,brightness};
-    Set_Light_Brightness(light);
+    Book_Lamp_Set_Light(light);
 }
 
 void Event_Turn_Off_Handler( void )
@@ -54,38 +67,29 @@ void Event_Change_Brightness_Handler( void )
     static uint8_t white;
     static uint8_t yellow;
     
-    if ( i >= sizeof ( set_brightness))
+    if ( i >= ARRAYSIZE ( set_brightness))
     {
         i = 0;
     }
     brightness = set_brightness[i++];
     
-    light_brightness.white = (brightness * light_ratio.white)/ ( light_ratio.white + light_ratio.yellow);
-            
-    light_brightness.yellow = (brightness * light_ratio.yellow)/ ( light_ratio.white + light_ratio.yellow);;
-    
-    
-    Set_Light_Brightness(light_brightness);
+    Book_Lamp_Set_Light ( light_brightness);
 }
 
 
 void Event_Change_Temperature_Handler( void )
 {
-    static uint8_t i = 0;
-    static uint8_t white;
-    static uint8_t yellow;
+    static uint8_t i = 1;
     
-    if ( i >= sizeof ( light_temperature_ratio))
+    if ( i >= ARRAYSIZE ( light_temperature_ratio))
     {
         i = 0;
     }
+  
     light_ratio = light_temperature_ratio[i++];
     
-    light_brightness.white = (brightness * light_ratio.white)/ ( light_ratio.white + light_ratio.yellow);
-            
-    light_brightness.yellow = (brightness * light_ratio.yellow)/ ( light_ratio.white + light_ratio.yellow);;
-    
-    Set_Light_Brightness(light_brightness);  
+    Book_Lamp_Set_Light ( light_ratio);
+
 }
 
 event_t Get_Event ( void )
@@ -98,8 +102,8 @@ event_t Get_Event ( void )
     
     static uint8_t start_repeat = 0;
     static time_t hold_time_start;
-    time_t const hold_timeout = 1000;
-            
+    time_t const hold_timeout = 800;
+          
     Button_Scan( 0 );
 
     sdouble_click_state = double_click_state;
@@ -109,11 +113,9 @@ event_t Get_Event ( void )
     {
         event = EVENT_DOUBLE_CLICK; 
     }
-
+    
     if ( Hold_Button_Run() == STATE_SUCCESS )
-    {
-
-        
+    {       
         if ( start_repeat )
         {
             if (Time_Passed(hold_time_start) > hold_timeout)
@@ -148,6 +150,7 @@ void Book_Lamp_Init ( void )
 {
     brightness = set_brightness[0];
     light_ratio = light_temperature_ratio[0];
+    Event_Turn_Off_Handler();
 }
 
 void Book_Lamp_App ( void )
@@ -171,7 +174,7 @@ void Book_Lamp_App ( void )
             if ( event == EVENT_BUTTON_RELEASED )
             {
                 Event_Change_Temperature_Handler();
-                state = STATE_CHANGE_LIGHT_BRIGTHNESS;                  
+                state = STATE_CHANGE_LIGHT_TEMPERATURE;                  
             }
             if (event == EVENT_DOUBLE_CLICK)
             {
@@ -183,7 +186,7 @@ void Book_Lamp_App ( void )
         case STATE_CHANGE_LIGHT_BRIGTHNESS:
             if ( event == EVENT_BUTTON_RELEASED )
             {
-                state = STATE_CHANGE_LIGHT_BRIGTHNESS;               
+                state = STATE_CHANGE_LIGHT_TEMPERATURE;               
             }
             
             if ( event == EVENT_CHANGE_BRIGHTNESS )
