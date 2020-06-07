aecbc6cb0b24609f92836f4fde58ffc2e823953b
jpnbino <jpnogg@gmail.com>
2020-06-07 23:19:32 +0200

    Added documentation for Wrapper functions. Optimizations set to -O2

--- LED_lamp.X/main.c
+++ LED_lamp.X/main.c
@@ -11,13 +11,36 @@
 #include "systick.h"
 #include "light.h"
 
-//PWM Wrappers
-void PWM_White_Set (uint8_t duty_cycle )
+/**
+ @brief Wrapper function comply with HAL function. 
+
+ This function is a wrapper to comply the HAL function to control the PWM to 
+ the function that controls the light intensity of the white light.
+ 
+ @param [in]   PWM Duty Cycle for white color
+ @return None
+
+ @see Soft_PWM_Set_Duty
+ @see Set_Light_Brightness
+*/
+void PWM_White_Light_Set (uint8_t duty_cycle )
 {
     Soft_PWM_Set_Duty(PWM_CHANNEL1, duty_cycle);
 }
 
-void PWM_Yellow_Set (uint8_t duty_cycle )
+/**
+ @brief Wrapper function comply with HAL function. 
+
+ This function is a wrapper to comply the HAL function to control the PWM to 
+ the function that controls the light intensity of the yellow light.
+ 
+ @param [in]   PWM Duty Cycle for yellow color
+ @return None
+
+ @see Soft_PWM_Set_Duty
+ @see Set_Light_Brightness
+*/
+void PWM_Yellow_Light_Set (uint8_t duty_cycle )
 {
     Soft_PWM_Set_Duty(PWM_CHANNEL2, duty_cycle);
 }
@@ -27,20 +50,22 @@ void PWM_Yellow_Set (uint8_t duty_cycle )
  */
 void main(void)
 {    
-    // initialize the device
+    /* Initializes the Lower level drivers and system. Provided by manufacturer. */
     SYSTEM_Initialize();
+ 
+    /* Set interrupt callback function */
+    TMR0_SetInterruptHandler(ISR_PWM_Callback);
+    TMR1_SetInterruptHandler(ISR_Systick_Callback);
+    TMR2_SetInterruptHandler(ISR_Button_Debounce_Callback);
     
+    /* Init application drivers */
     Button_Driver_Init();
     Soft_PWM_Init();
     Systick_Init();
     
-    TMR0_SetInterruptHandler(ISR_PWM_Callback);
-    TMR1_SetInterruptHandler(ISR_Systick_Callback);
-    TMR2_SetInterruptHandler(ISR_Button_Debounce_Callback);
-
     light_init_t light_init;
-    light_init.white_color_pwm_set = &PWM_White_Set;
-    light_init.yellow_color_pwm_set = &PWM_Yellow_Set;
+    light_init.white_color_pwm_set = &PWM_White_Light_Set;
+    light_init.yellow_color_pwm_set = &PWM_Yellow_Light_Set;
     Light_Init(light_init);
  
     INTERRUPT_GlobalInterruptEnable();
@@ -48,14 +73,12 @@ void main(void)
 
     Book_Lamp_Init();
 
-    Button_Scan(BUTTON_CHANNEL_0);
-    DELAY_milliseconds(20);
-    Button_Scan(BUTTON_CHANNEL_0);
-    Button_Clear_Events();
-
     while (1)
     {
+        /* Clear Watchdog */
         CLRWDT();
+        
+        /* run application */
         Book_Lamp_App();
     }
 }
